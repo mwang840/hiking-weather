9f030c4ffe5fdf7ed0627afca67bdd62ace81f7b
Maxwell Wang <maxwang@udel.edu>
2023-03-30 11:25:27 -0400

    date

--- src/HikingWeather.jsx
+++ src/HikingWeather.jsx
@@ -2,3 +2,7 @@ import React, { useState, useEffect } from "react";
 import configuration from "./config.json";
 import { HikingWeatherInfo } from "./Information";
 import axios from "axios";
+
+const callWeatherApi = async(place)=>{
+    const currentTime = new Date();
+}
