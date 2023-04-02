38dc1d157bf58802721e85a8d4c57176567294d1
Maxwell Wang <maxwang@udel.edu>
2023-04-02 15:07:51 -0400

    i'll explain in a bit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from "./logo.svg";
 import Weather from "./Weather.jsx";
 import "./App.css";
-
+import hiking from "./assets/hiking.jpg";
+import quotes from "./assets/hikign quotes.jpg";
 function App() {
     return (
-        <Weather/>
+        <div>
+            <h1>Enter a location to see if the weather is good for hiking!</h1>
+            <img src={quotes} alt="Hiking Quotes" width="300" height="200"></img>
+            <Weather/>
+            <img src={hiking} alt="Hiking" width="300" height="200"/>
+        </div>
+        
     );
 }
 
